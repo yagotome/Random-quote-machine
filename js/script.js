1115bdca59fa08b81f0e3c80cb76cc425de389bf
Yago Gomes Tomé de Sousa <yagotome_1996@hotmail.com>
2016-02-06 21:13:44 -0200

    Random quote machine v1.0

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function() {
+	var quotes = [
+		{		
+			phrase: "Whatever the mind of man can conceive and believe, it can achieve.",
+			author: "Napoleon Hill"
+		},
+		{		
+			phrase: "Your time is limited, so don’t waste it living someone else’s life.",
+			author: "Steve Jobs"
+		},
+		{		
+			phrase: "Winning isn’t everything, but wanting to win is.",
+			author: "Vince Lombardi"
+		},
+		{		
+			phrase: "Try not to become a man of success, but rather try to become a man of value.",
+			author: "Albert Einstein"
+		},
+		{		
+			phrase: "We all live under the same sky, but we don't all have the same horizon.",
+			author: "Konrad Adenauer"
+		},
+		{		
+			phrase: "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+			author: "Bill Gates"
+		}
+	];
+
+	function randInt(a, b) {
+		return a + Math.floor(Math.random()*(b-a+1));
+	}
+
+	$("#btnNew").click(function() {
+		var i = randInt(0, quotes.length - 1);
+		$("#quote-phrase").text(quotes[i].phrase);
+		$("#quote-author").text(quotes[i].author);
+	});
+
+});
+
